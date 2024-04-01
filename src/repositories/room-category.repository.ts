2aff4199c09907be596b48695139e77f0ec465a5
vs <valeriisklyarov@gmail.com>
2024-04-01 12:15:47 +0300

    Fix data source import in RoomCategoryRepository

--- src/repositories/room-category.repository.ts
+++ src/repositories/room-category.repository.ts
@@ -1,6 +1,6 @@
 import {inject} from '@loopback/core';
 import {DefaultCrudRepository} from '@loopback/repository';
-import {Error: bad inputDataSource} from '../datasources';
+import {LocalMysqlDataSource} from '../datasources';
 import {RoomCategory, RoomCategoryRelations} from '../models';
 
 export class RoomCategoryRepository extends DefaultCrudRepository<
@@ -9,7 +9,7 @@ export class RoomCategoryRepository extends DefaultCrudRepository<
   RoomCategoryRelations
 > {
   constructor(
-    @inject('datasources.') dataSource: Error: bad inputDataSource,
+    @inject('datasources.local_mysql') dataSource: LocalMysqlDataSource
   ) {
     super(RoomCategory, dataSource);
   }
