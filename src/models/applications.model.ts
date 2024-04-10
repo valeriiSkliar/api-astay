ee80731711eecb62f5f1eafe43065ad5fd6fc3cf
vs <valeriisklyarov@gmail.com>
2024-04-10 11:04:05 +0300

    Update regex patterns in Applications model

--- src/models/applications.model.ts
+++ src/models/applications.model.ts
@@ -20,7 +20,7 @@ export class Applications extends Entity {
     jsonSchema: {
       minLength: 5,
       maxLength: 50,
-      pattern: '^[a-zA-Zа-яА-Я0-9.,!?;:\\-\'"\\s]+$',
+      pattern: '^[a-zA-Zа-яА-Я0-9.,!?;:\-\'"\s]+$',
     }
   })
   name: string;
@@ -50,7 +50,7 @@ export class Applications extends Entity {
     jsonSchema: {
       minLength: 10,
       maxLength: 2000,
-      pattern: '^[a-zA-Zа-яА-Я0-9.,!?;:\\-\'"\\s]+$',
+      pattern: '^[a-zA-Zа-яА-Я0-9.,!?;:\-\'"\s]+$',
     }
   })
   message?: string;
