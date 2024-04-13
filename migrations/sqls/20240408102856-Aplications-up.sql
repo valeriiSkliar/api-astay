4cce23e2b2689d552e9caf868feaaf3a31cf3c11
vs <valeriisklyarov@gmail.com>
2024-04-13 18:53:17 +0300

    Refactor SQL queries in migrations/sqls/20240408102856-Aplications-up.sql and migrations/sqls/20240411115425-Transfers-up.sql

--- migrations/sqls/20240408102856-Aplications-up.sql
+++ migrations/sqls/20240408102856-Aplications-up.sql
@@ -1,11 +1,11 @@
 INSERT INTO Applications (email, phone, name, message, isProcessed, isOpened, pageName) VALUES 
-    ('Emely_Morar46@gmail.com', '38095-816-624', 'Lazaro Conroy', 'Aequitas uterque.', '0', '0', 'complexes'),
-('Toby23@hotmail.com', '38095-591-829', 'Adelle Weissnat', 'Uredo vae.', '0', '0', 'complexes'),
-('Josephine57@hotmail.com', '38095-236-933', 'Mary Lueilwitz', 'Voluptates tego.', '1', '0', 'main'),
-('Esta_Swift86@hotmail.com', '38095-859-693', 'Immanuel Rohan', 'Concido asper.', '1', '0', 'abousUs'),
-('Peter10@yahoo.com', '38095-400-510', 'Jany Leffler', 'Dolores attero.', '0', '1', 'abousUs'),
-('Justen_Konopelski@hotmail.com', '38095-246-348', 'Blanca Aufderhar', 'Calculus commodi.', '1', '1', 'complexes'),
-('Sage.Carter83@yahoo.com', '38095-396-462', 'Kylee Boyer', 'Carpo tantum.', '0', '0', 'abousUs'),
-('Ernesto.Dibbert@gmail.com', '38095-634-017', 'Boris Rempel', 'Conqueror est.', '0', '0', 'main'),
-('Trudie.Johns58@yahoo.com', '38095-751-445', 'Charlene Will', 'Crepusculum desipio.', '1', '0', 'complexes'),
-('Elroy27@hotmail.com', '38095-470-702', 'Jazlyn Moore', 'Turba studio.', '1', '1', 'abousUs');
+    ('Pinkie_Abernathy@hotmail.com', '38095-065-264', 'Bennett Kihn', 'Ager quibusdam.', '0', '0', 'abousUs'),
+('Lucinda89@hotmail.com', '38095-055-764', 'Lawson Hagenes', 'Repudiandae carmen.', '1', '1', 'forOwners'),
+('Letha.Stroman56@yahoo.com', '38095-775-117', 'Emilio Bechtelar', 'Vulnus trado.', '1', '0', 'abousUs'),
+('Dale_Ankunding@gmail.com', '38095-608-494', 'Margarete Effertz', 'Ver rerum.', '0', '1', 'complexes'),
+('Raven.Kreiger34@yahoo.com', '38095-568-080', 'Norwood Ondricka', 'Adfero vitiosus.', '0', '0', 'complexes'),
+('Blanca.Mohr@hotmail.com', '38095-710-779', 'Augustus Hessel', 'Summopere suadeo.', '1', '0', 'main'),
+('Ludwig.Halvorson80@gmail.com', '38095-670-119', 'Ari Pouros', 'Suus thesaurus.', '1', '0', 'main'),
+('Walter_Mosciski35@yahoo.com', '38095-269-885', 'Sadye Bogisich', 'Taceo ara.', '1', '0', 'forOwners'),
+('Cyrus_Luettgen36@yahoo.com', '38095-375-891', 'Lucile Marquardt', 'Consectetur tempora.', '1', '1', 'abousUs'),
+('Damian8@yahoo.com', '38095-387-473', 'Madelynn McClure', 'Aptus fugit.', '1', '0', 'complexes');
--- migrations/sqls/20240411115425-Transfers-up.sql
+++ migrations/sqls/20240411115425-Transfers-up.sql
@@ -1,11 +1,11 @@
-INSERT INTO Transfer (type, date, flightNumber, nameOfSignage, guests, amountBags, phone, city, departure, time, comments, status, price, discount) VALUES 
-('arrival', '2024-07-17', 'Drew', 'Rohan Alia', 1, 1, 292426178, 'Kokomo', 'JFK', '2024-10-19', 'Assumenda uredo tot ustulo.', 0, 15, 0),
-('Departure', '2025-01-25', 'River', 'Sipes Kayden', 1, 0, 490310342, 'Woodbury', 'JFK', '2024-07-10', 'Socius iste tabella conqueror.', 0, 0, 0),
-('Departure', '2024-11-08', 'Kai', 'Mann Julian', 0, 0, 172584919, 'Wymanstead', 'JFK', '2024-05-27', 'Tenax utilis harum strenuus.', 0, 1, 1),
-('Departure', '2024-06-10', 'Parker', 'Crona Mercedes', 1, 0, 221448521, 'Frederick', 'JFK', '2024-07-30', 'Deputo cilicium quidem atrox.', 0, 0, 0),
-('Arrival', '2024-10-17', 'Skyler', 'Boyle Hattie', 0, 0, 956444542, 'Oberbrunnercester', 'JFK', '2025-01-20', 'Sit callide adduco virgo.', 0, 0, 0),
-('Arrival', '2024-11-04', 'Phoenix', 'Will-Waters Mark', 1, 1, 154273833, 'West Edward', 'JFK', '2024-06-23', 'Stips aspicio iure tamdiu.', 0, 1, 1),
-('Departure', '2024-09-20', 'Dakota', 'Rippin Alycia', 0, 0, 249054271, 'Lake Garretfield', 'JFK', '2024-09-06', 'Toties advenio venustas decipio.', 0, 1, 1),
-('Arrival', '2024-12-19', 'Taylor', 'Luettgen Effie', 1, 0, 424800396, 'Port Nicholaus', 'JFK', '2024-09-30', 'Suscipio supra accedo cumque.', 0, 1, 0),
-('Departure', '2025-04-07', 'Kennedy', 'Barrows Velva', 1, 1, 552242740, 'Hilbertcester', 'JFK', '2024-08-01', 'Viriliter autus civitas commemoro.', 0, 1, 0),
-('Departure', '2024-10-12', 'Brooklyn', 'Rath Kadin', 0, 0, 278200412, 'Kavonberg', 'JFK', '2024-11-15', 'Ab derideo admoveo basium.', 0, 0, 1);
+INSERT INTO Transfer (type, date, flightNumber, nameOfSignage, guests, amountBags, phone, city, departure, time, comments, status, isArchived, price, discount) VALUES 
+('arrival', '2024-06-10', 'Ryan', 'Schoen Abagail', 0, 0, 340212044, 'East Maevecester', 'JFK', '2024-08-23', 'Tertius architecto aeternus utrum.', 0, 0, 59, 1),
+('Arrival', '2024-08-25', 'Logan', 'Hammes Brianne', 0, 1, 667994314, 'New Tannermouth', 'JFK', '2024-11-09', 'Arcus alias deserunt pariatur.', 0, 0, 1, 1),
+('Arrival', '2024-12-02', 'Emerson', 'Wisozk Tatyana', 1, 0, 948039969, 'Randifort', 'JFK', '2025-03-05', 'Vesper voluptate crinis demens.', 0, 0, 1, 0),
+('Arrival', '2024-10-15', 'Dakota', 'Walter Quincy', 0, 1, 406645619, 'Bednarberg', 'JFK', '2024-05-07', 'Supplanto aegrotatio claustrum aetas.', 0, 0, 0, 1),
+('Arrival', '2025-03-17', 'Brooklyn', 'Wilderman Joanny', 0, 0, 410913628, 'Coon Rapids', 'JFK', '2025-03-06', 'Absens tabgo amplexus super.', 0, 0, 1, 0),
+('Arrival', '2024-06-25', 'Austin', 'Welch Jensen', 0, 1, 585002202, 'San Clemente', 'JFK', '2025-03-27', 'Nihil combibo velociter eos.', 0, 0, 1, 1),
+('Arrival', '2024-07-29', 'Charlie', 'Bayer Theodora', 1, 1, 310138353, 'Quintonstad', 'JFK', '2024-09-14', 'Vita comburo considero alveus.', 0, 0, 0, 1),
+('Departure', '2024-06-09', 'Dakota', 'Effertz Susanna', 1, 0, 964797241, 'Fort Berneice', 'JFK', '2024-10-20', 'Vis volutabrum apparatus unus.', 0, 0, 1, 0),
+('Departure', '2024-09-15', 'Robin', 'Cremin Clay', 1, 0, 525747436, 'South Durward', 'JFK', '2024-12-07', 'Libero creber congregatio vereor.', 0, 0, 0, 1),
+('Arrival', '2024-11-24', 'Emerson', 'Howe-Schinner Lysanne', 0, 0, 715343263, 'Schultzboro', 'JFK', '2024-12-26', 'Abscido patria defaeco adhaero.', 0, 0, 1, 0);
