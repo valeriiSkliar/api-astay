3df369fefb218a843650d7157a86dfb54c3e235c
vs <valeriisklyarov@gmail.com>
2024-04-25 10:23:35 +0300

    Update Locations model properties

--- src/models/locations.model.ts
+++ src/models/locations.model.ts
@@ -10,23 +10,23 @@ export class Locations extends Entity {
   })
   id?: number;
 
-  @property({
-    type: 'string',
-    required: true,
-  })
-  name: string;
+  // @property({
+  //   type: 'string',
+  //   required: true,
+  // })
+  // name: string;
 
-  @property({
-    type: 'string',
-    required: true,
-  })
-  city: string;
+  // @property({
+  //   type: 'string',
+  //   required: true,
+  // })
+  // city: string;
 
-  @property({
-    type: 'string',
-    default: 'Thailand',
-  })
-  country?: string;
+  // @property({
+  //   type: 'string',
+  //   default: 'Thailand',
+  // })
+  // country?: string;
 
   @property({
     type: 'object',
